febc189103d13d26a85ad94e3c4f54a8cbe59157
PedroGuerra8 <pguerra872@gmail.com>
2025-03-17 20:24:13 -0300

    up
    17.03

---
+++ aula07/app_aula07/lib/main.dart
@@ -0,0 +1,86 @@
+// biblioteca material que vai permitir a criação dos widgets para o Flutter no Android
+import 'package:flutter/material.dart';
+
+// void main função principal  do app
+void main() {
+  // runApp função que constroi o app chamando a classe do tipo Stateless
+  runApp(Telapp());
+}
+
+class Telapp extends StatelessWidget {
+  const Telapp({super.key});
+
+// override polimorfismo, permite tratar objetos de diferentes formas
+  @override
+  Widget build(BuildContext context) {
+    // MaterialApp faz parte do design do aplicativo
+    return MaterialApp(
+      // Remove a faixa de debug do app
+      debugShowCheckedModeBanner: false,
+      home: Column(
+        // alinhamento primário
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisAlignment: MainAxisAlignment.end,
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+        Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(width: 300, height: 100, color: Colors.blue,),
+          Container(width: 280, height: 80, color: Colors.red,
+          child: Text("Senai", textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            decoration: TextDecoration.none,
+            fontSize: 25,
+            ),),),
+          ],
+          ),
+        
+        
+        Container(width: 300, height: 100, color: Colors.red,
+        child: Text("Senai - Mobile", style: TextStyle(fontSize: 30,
+        color: Colors.white,
+        decoration: TextDecoration.none),
+        ),),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(width: 50, height: 50, color: Colors.amber,),
+            Container(width: 50, height: 50, color: Colors.white,),
+            Container(width: 50, height: 50, color: Colors.grey,),
+          ],
+        ),
+        // Widget botao
+        // on pressed evento que ocorre quando o botao é pressionado
+        ElevatedButton(
+          onPressed: (){
+            print("Desenvolvimento Mobile 1");
+          }, child: Text("Mensagem")),
+
+        Stack(
+          //propriedade de alinhamento
+            alignment: Alignment.center,
+          children: [
+            Container(width: 300, height: 100, color: Colors.blue,),
+            Container(width: 200, height: 80, color: Colors.red,
+            child: Text("Pedro", textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              decoration: TextDecoration.none,
+              fontSize: 25,
+
+
+            ),),),
+            
+          ],
+        ),
+        // Text permite inserir texto no aplicativo
+        
+        ],
+      ),
+    );
+  }
+}
