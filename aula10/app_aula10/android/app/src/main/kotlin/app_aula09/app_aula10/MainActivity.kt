a873718451b9e3bae9366f4388807ddd45b858fe
PedroGuerra8 <pguerra872@gmail.com>
2025-04-07 20:57:24 -0300

    app aula 10

---
+++ aula10/app_aula10/android/app/src/main/kotlin/app_aula09/app_aula10/MainActivity.kt
@@ -0,0 +1,5 @@
+package app_aula09.app_aula10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ aula10/teste_im/android/app/src/main/kotlin/app_aula09/teste_im/MainActivity.kt
@@ -0,0 +1,5 @@
+package app_aula09.teste_im
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
